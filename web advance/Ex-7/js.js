188b8ff9cd47581a232b0a7b1fd8012097d68929
Vanh1902 <70436782+Vanh1902@users.noreply.github.com>
2021-01-10 08:49:38 +0700

    web

---
+++ web advance/Ex-7/js.js
@@ -0,0 +1,11 @@
+<script>
+// let age = 16;
+// localStorage.setItem("UserAge", age);
+// let UserAge = localStorage.getItem("UserAge");
+// console.log(UserAge)
+
+localStorage.setItem("coursename","HBT");
+let coursename = localStorage.getItem("coursename");
+console.log(coursename);
+
+</script>
