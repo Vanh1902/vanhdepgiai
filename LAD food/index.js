73ee039f5e053176015d2ff2b404454a121145b6
Vanh1902 <70436782+Vanh1902@users.noreply.github.com>
2021-03-10 19:19:25 +0700

    DAM
    code

---
+++ LAD food/index.js
@@ -0,0 +1,59 @@
+function signup() {
+    let input_email = document.getElementById("email").value
+    let input_name = document.getElementById("name").value
+    let input_password = document.getElementById("password").value
+
+
+    let caseblock = document.getElementById("checkCase")
+
+    let listUsers = [];
+    let listPassword = [];
+    let listUsername = [];
+    let isaccountExist = false;
+    if (JSON.parse(localStorage.getItem("listUsers") != null)) {
+        listUsers = JSON.parse(localStorage.getItem("listUsers"))
+    }
+    if (JSON.parse(localStorage.getItem("listPassword") != null)) {
+        listPassword = JSON.parse(localStorage.getItem("listPassword"))
+    }
+    if (JSON.parse(localStorage.getItem("listUsername") != null)) {
+        listUsername = JSON.parse(localStorage.getItem("listUsername"))
+    }
+
+
+    if (input_email == "" || input_password == "" || input_name == "") {
+        console.log("ERROR")
+        caseblock.innerHTML = "Sign up fail"
+        caseblock.style.color = "red"
+    }
+    else {
+        for (let i = 0; i < listUsers.length; i++) {
+            if (input_email == listUsers[i]) {
+                console.log("ERROR")
+                caseblock.innerHTML = "account already have"
+                caseblock.style.color = "red"
+                isaccountExist = true
+            }
+        }
+
+        if (email.value.includes("@") == true) {
+            if (!isaccountExist) {
+                listUsers.push(input_email);
+                listPassword.push(input_password);
+                listUsername.push(input_name);
+
+                localStorage.setItem("listUsers", JSON.stringify(listUsers))
+                localStorage.setItem("listPassword", JSON.stringify(listPassword))
+                localStorage.setItem("listUsername", JSON.stringify(listUsername))
+
+                console.log("SUCCESFUL")
+                caseblock.innerHTML = "Sign up sucess"
+                caseblock.style.color = "green" 
+                location.replace("login.html")
+            }
+
+
+        }
+    }
+
+}
---
+++ LAD food/index2.js
@@ -0,0 +1,35 @@
+function login(){
+    let input_email = document.getElementById("username").value;
+    let input_password = document.getElementById("password").value;
+    let caseblock = document.getElementById("checkCase");
+
+    let listEmail = JSON.parse(localStorage.getItem("listUsers"));
+    let listPassword = JSON.parse(localStorage.getItem("listPassword"));
+
+    for(let i = 0;i<listEmail.length && i< listPassword.length; i++){
+        if(input_email == listEmail[i] && input_password ==listPassword[i]){
+        index = i
+        console.log(index)
+        localStorage.setItem("listEmail", index)
+        console.log("sign in suc")
+        caseblock.innerHTML = "sign in suc"
+        caseblock.style.color= "green"
+        location.replace("Home.html")
+
+        }
+        else{       
+        console.log("error")
+        caseblock.innerHTML = "fail"
+        caseblock.style.color ="red"
+    }
+    
+    }
+}
+function loadfunction(){
+    if (localStorage.getItem("listEmail")!= null){
+        let user = JSON.parse(localStorage.getItem("listEmail"));
+        let username = JSON.parse(localStorage.getItem("listUsername"));
+        document.getElementById("demo").innerHTML= username[Number(user)]
+    };
+
+}
